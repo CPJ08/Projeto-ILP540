6923f70aec3513dd0ad11c05ad73662d3944a3ba
CPJ08 <91169776+CPJ08@users.noreply.github.com>
2022-03-24 12:22:51 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,66 +6,37 @@
 	<title>Home</title>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap.min.css" media="screen" />
         <link href="css/background.css">
+		<link rel="stylesheet" href="css/button.css">
+		<link rel="stylesheet" href="css/Ftela.css">
+        <link rel="stylesheet" href="css/Login.css">
+        >
 </head>
 <body>
-	<div>
-		<ul>
-			<li><a href="#login" accesskey="1">[1] Login</a></li>
-			<li><a href="#menu" accesskey="2">[2] Menu</a></li>	
-			<li><a href="#principal" accesskey="3">[3] Principal</a></li>
-			<li><a href="#rodape" accesskey="4">[4] Rodape</a></li>
-		</ul>
-	</div>
-	<header>
-		<h1>Meu App</h1>
-		<div id="login">
-			<form action="#" method="post">
-				<label>
-					Usuario:<input type="text" name="usuario" id="usuario">
-				</label>
-				<label>
-					Senha:<input type="password" name="senha" id="senha">
-				</label>
-			</form>
-		</div>
-		<nav id="menu"> 
-			<ul>
-				<li><a href="index.html">Home</a></li>
-				<li><a href="contato.html">Contato</a></li>
-			</ul>
-		</nav>
-	</header>
-	<main id="principal">
-		  <div class="alert alert-primary" role="alert">
-		    Um simples alerta primary. Olha só!
-		  </div>
-		  <div class="alert alert-secondary" role="alert">
-		    Um simples alerta secondary. Olha só!
-		  </div>
-		  <div class="alert alert-success" role="alert">
-		    Um simples alerta success. Olha só!
-		  </div>
-		  <div class="alert alert-danger" role="alert">
-		    Um simples alerta danger. Olha só!
-		  </div>
-		  <div class="alert alert-warning" role="alert">
-		    Um simples alerta warning. Olha só!
-		  </div>
-		  <div class="alert alert-info" role="alert">
-		    Um simples alerta info. Olha só!
-		  </div>
-		  <div class="alert alert-light" role="alert">
-		    Um simples alerta light. Olha só!
-		  </div>
-		  <div class="alert alert-dark" role="alert">
-		    Um simples alerta dark. Olha só!
-		  </div>
-	</main>
-	<footer id="rodape">
-		<p>Copyright © 2022</p>
-	</footer>
-		    <script src="js/jquery-3.3.1.slim.min.js" ></script>
-		    <script src="js/popper.min.js" ></script>
-		    <script src="js/bootstrap.min.js" ></script>
+    <div>
+        <img src="medi.jpg" alt="Imagem Medicina" width="500px" border-radius="20px">
+    </div>
+    <div id="divborda" >
+        <p class="h1"><u>Login</u></p>
+        <br><br>
+        <label>
+            <p class="h5">Email:</p> <input placeholder="Digite seu email" type="text" name="usuario" id="usuario">
+        </label>
+        <br>
+
+        <label>
+            <p class="h5">Senha:</p><input placeholder="Digite sua senha" type="password" name="senha" id="senha">
+        </label>
+        <br>
+        <button class="button">Cadastrar</button>
+        <button class="button">Logar</button>
+        <br />
+        <a href=""><p>Esqueceu a Senha?</p></a>
+        
+    </div> 
+    <script src="js/jquery-3.3.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
 </body>
+ 
 </html>
