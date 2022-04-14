3c7cffa138bd3cf1e6ad0ce1a3472a90534be371
CPJ08 <91169776+CPJ08@users.noreply.github.com>
2022-04-14 11:50:00 -0300

    Update Logado.php

--- Logado.php
+++ Logado.php
@@ -12,12 +12,12 @@
 </head>
     <body>
         <div>
-            <a href="EditarDados.html"><img src="Imagem/conta.png" class="imge" alt="Imagem ContaEditar" width="130px" border-radius="50px"></a>
-            <a href="Login.html"><img src="Imagem/logout.png" class="imgl" alt="Imagem Logout" width="70px" border-radius="50px"></a><br>
-            <a href="Resultado.html"><img src="Imagem/resultado.png" class="imgresultado" alt="Imagem Resultado Exame" width="100px" border-radius="50px"></a>
-            <a href="Agendamento.html"><img src="Imagem/agendamento.png" class="imgagendamento" alt="Imagem Agendamento" width="100px" border-radius="50px"></a><br>
-            <a href="Bancodesangue.html"><img src="Imagem/bancodesangue.png" class="imgbanco" alt="Imagem Banco De Sangue" width="150px" border-radius="50px"></a>
-            <a href="Atendimento.html"><img src="Imagem/atendimento.png" class="imgatendimento" alt="Imagem Atendimento" width="150px" border-radius="50px"></a>
+            <a href="EditarDados.php"><img src="Imagem/conta.png" class="imge" alt="Imagem ContaEditar" width="130px" border-radius="50px"></a>
+            <a href="index.php"><img src="Imagem/logout.png" class="imgl" alt="Imagem Logout" width="70px" border-radius="50px"></a><br>
+            <a href="Resultado.php"><img src="Imagem/resultado.png" class="imgresultado" alt="Imagem Resultado Exame" width="100px" border-radius="50px"></a>
+            <a href="Agendamento.php"><img src="Imagem/agendamento.png" class="imgagendamento" alt="Imagem Agendamento" width="100px" border-radius="50px"></a><br>
+            <a href="Bancodesangue.php"><img src="Imagem/bancodesangue.png" class="imgbanco" alt="Imagem Banco De Sangue" width="150px" border-radius="50px"></a>
+            <a href="Atendimento.php"><img src="Imagem/atendimento.png" class="imgatendimento" alt="Imagem Atendimento" width="150px" border-radius="50px"></a>
         </div>
             <script src="js/jquery-3.3.1.slim.min.js"></script>
             <script src="js/popper.min.js"></script>
